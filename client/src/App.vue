ee7683a37cdd866dc6f8b9cea2f8f98604c37d76
Sefa Ekici <40717655+sefaekici@users.noreply.github.com>
2021-06-18 18:15:19 +0300

    Client Navbar Design Navbar design and responsive with scss

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <app-navbar></app-navbar>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Rubik",sans-serif;
   }
-}
+
+  #app{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 </style>
+
+
+<script>
+import Navbar from "./components/Navbar.vue"
+export default {
+  components:{
+    appNavbar:Navbar,
+  },
+}
+</script>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="navbar">
+        <h4>User App</h4>
+
+        <ul>
+            <li><router-link to="/add-user">Add User</router-link></li>
+            <li><router-link to="/user-list">User List</router-link></li>
+        </ul>
+
+    </div>
+</template>
+
+
+
+<style lang="scss">
+    .navbar{
+        width: 100%;
+        height: 85px;
+        background: #293B5F;
+        margin-top: 0;
+        padding: 15px 5rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        color:#fff;
+
+        @media screen and (max-width:650px) {
+            flex-direction: column;
+            height: 95px;
+            h4{
+                margin-bottom: 15px;
+            }
+
+        }
+
+        h4{
+            font-size: 25px;
+        }
+        ul{
+            display: flex;
+            li{
+                list-style: none;
+                &:first-child{
+                    margin-right: 30px;
+                }
+                a{
+                    text-decoration: none;
+                    color: black;
+                    font-size: 20px;
+                    color:#fff;
+                    &:hover{
+                        opacity: 0.9;
+                    }
+                }
+            }
+        }
+    }
+</style>
