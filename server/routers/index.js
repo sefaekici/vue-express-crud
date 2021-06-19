84a44401b2ff8a769e48718124fd7a6cf055fc7c
Sefa Ekici <40717655+sefaekici@users.noreply.github.com>
2021-06-19 13:16:03 +0300

    Crash Fix On Routers.js

--- server/routers/index.js
+++ server/routers/index.js
@@ -14,3 +14,5 @@ router.get("/users", getAllUsers);
 router.get("/users/:id", getUserById);
 router.put("/users/:id", updateUserById);
 router.delete("/users/:id", deleteUserById);
+
+module.exports = router;
